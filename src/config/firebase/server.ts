41d10a81507cc343349a87fbfe4f40e62105996a
Rafael <rafa.shiga@gmail.com>
2021-04-11 17:54:17 -0300

    fix: add space new line from firebase private key

--- src/config/firebase/server.ts
+++ src/config/firebase/server.ts
@@ -1,13 +1,14 @@
-import admin from 'firebase-admin'
+import admin from 'firebase-admin';
 
 export const firebaseServer = admin.apps.length
-      ? admin.app()
-      : admin.initializeApp({
-        credential: admin.credential.cert({
-          projectId: process.env.FIREBASE_SERVER_PROJECT_ID,
-          privateKey: process.env.FIREBASE_SERVER_PRIVATE_KEY,
-          clientEmail: process.env.FIREBASE_SERVER_CLIENT_EMAIL,
-        })
-      });
-
-
+  ? admin.app()
+  : admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_SERVER_PROJECT_ID,
+        privateKey: process.env.FIREBASE_SERVER_PRIVATE_KEY.replace(
+          /\\n/g,
+          '\n'
+        ),
+        clientEmail: process.env.FIREBASE_SERVER_CLIENT_EMAIL,
+      }),
+    });
