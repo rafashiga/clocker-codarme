986c4be6e12ece80da4aa8f17bd37db07b810cae
Rafael <rafa.shiga@gmail.com>
2021-04-11 22:11:53 -0300

    fix: adjust env

--- src/config/firebase/server.ts
+++ src/config/firebase/server.ts
@@ -5,7 +5,10 @@ export const firebaseServer = admin.apps.length
   : admin.initializeApp({
       credential: admin.credential.cert({
         projectId: process.env.FIREBASE_SERVER_PROJECT_ID,
-        privateKey: process.env.FIREBASE_SERVER_PRIVATE_KEY,
+        privateKey: process.env.FIREBASE_SERVER_PRIVATE_KEY.replace(
+          /\\n/g,
+          '\n'
+        ),
         clientEmail: process.env.FIREBASE_SERVER_CLIENT_EMAIL,
       }),
     });
